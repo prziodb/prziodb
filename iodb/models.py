dc4b00aa56a04addfcd74fba074f6b91a737fdf0
prziodb <prz.iodb@gmail.com>
2011-05-16 11:31:43 +0200

    User zostało skonfigurowane. Początkowa implementacja modeli danych

--- iodb/models.py
+++ iodb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Host(models.Model):
+    name = models.CharField(max_length=50)
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    submited_by = models.CharField(User)
--- settings.py
+++ settings.py
@@ -119,6 +119,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
 )
 
 # A sample logging configuration. The only tangible logging
