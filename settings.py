90b274d9d076234df0f61a0ed45f16ffb9175635
prziodb <prz.iodb@gmail.com>
2011-05-16 11:08:59 +0200

    Dodana poprawna konfiguracja serwisu i bd

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'iodb',                      # Or path to database file if using sqlite3.
+        'USER': 'iodb',                      # Not used with sqlite3.
+        'PASSWORD': 'pass|ssap',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -142,4 +142,4 @@ LOGGING = {
             'propagate': True,
         },
     }
-}
+    }
